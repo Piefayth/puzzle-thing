6c76e0858fb1459a48a2d6ac6d049b4b5668425a
Sean Ryan <piefayth@gmail.com>
2016-03-05 22:40:17 -0600

    board boundaries and cursor hiding

--- client/src/components/drag.js
+++ client/src/components/drag.js
@@ -2,25 +2,35 @@ import Component from 'components/component.js';
 
 function init(){
   this.sprite.interactive = true;
+  this.sprite.buttonMode = true;
+  this.sprite.defaultCursor = 'auto';
 
   this.sprite.on('mousedown', e => {
-    this._drag_held = true;
-    this.mousedownHandlers.forEach(handler => handler.call(this, e));
-    this.sprite.x = e.data.global.x;
-    this.sprite.y = e.data.global.y;
-  });
-
-  this.sprite.on('mousemove', e => {
-    if(this._drag_held){
-      this.dragHandlers.forEach(handler => handler.call(this, e));
+    if(!this._drag_held){
+      this.mousedownHandlers.forEach(handler => handler.call(this, e));
+      this._drag_held = true;
       this.sprite.x = e.data.global.x;
       this.sprite.y = e.data.global.y;
     }
   });
 
+  this.sprite.on('mousemove', e => {
+    // if we are dragging something and our current position is still inside the parent
+    if(this._drag_held && this.parent.sprite.containsPoint(e.data.global)){
+      this.dragHandlers.forEach(handler => handler.call(this, e));
+      this.sprite.x = e.data.global.x;
+      this.sprite.y = e.data.global.y;
+    } else if(this._drag_held){
+      this.releaseHandlers.forEach(handler => handler.call(this, e));
+      this._drag_held = false;
+    }
+  });
+
   this.sprite.on('mouseup', e => {
-    this.releaseHandlers.forEach(handler => handler.call(this, e));
-    this._drag_held = false;
+    if(this._drag_held){
+      this.releaseHandlers.forEach(handler => handler.call(this, e));
+      this._drag_held = false;
+    }
   });
 }
 
